debdc773553a284c7ca7f20a8237d3009f70647e
RchrdMgwtz <richard.mogwitz@htw-dresden.de>
2019-09-18 14:01:49 +0200

    Anlauttabelle wird angezeigt mit Checkboxen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,29 @@
     <h1>Welche Buchstaben kannst du schon?</h1>
     <div id="home_grid">
       <!--<img id="anlauttabelle" src="../assets/anlauttabelle.jpg" alt="Anlauttabelle" />-->
-      <div v-for="(entry, i) in piktArray" :key="entry.name">
-        {{ piktArray[i].upper }} - {{ piktArray[i].lower }}
-        <img :src="piktArray[i].img" />
+      <div
+        v-for="(entry, i) in piktArray"
+        :key="entry.name"
+        class="tabelle_wrapper"
+      >
+        <!-- Hier Kästchen erstellen -->
+        <div class="kaestchen_wrapper">
+          <div class="bild_wrapper">
+            <img :src="piktArray[i].img" />
+          </div>
+          <div class="info_wrapper">
+            <div class="buchstabe_wrapper">
+              {{ piktArray[i].upper }} {{ piktArray[i].lower }}
+            </div>
+            <div class="checkbox_wrapper">
+              <input type="checkbox" />
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
 
+    <div class="button_wrapper">
       <button id="button_puzzle" class="Piktobutton">
         <img id="puzzleimg" alt="Puzzle" src="../assets/puzzle.png" />
       </button>
@@ -65,7 +83,9 @@ export default {
 }*/
 
 #home_grid {
-  display: grid;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 
 #anlauttabelle {
@@ -86,5 +106,29 @@ export default {
 #button_start {
   grid-row: 2;
   grid-column: 3;
+}
+
+.kaestchen_wrapper {
+  display: flex;
+  flex-direction: column;
+  max-width: 100px;
+}
+
+.info_wrapper {
+  display: flex;
+  flex-direction: row;
+}
+
+.buchstabe_wrapper {
+  width: 66%;
+}
+
+.checkbox_wrapper {
+  width: 34%;
+}
+
+.button_wrapper {
+  display: flex;
+  flex-direction: row;
 }
 </style>
