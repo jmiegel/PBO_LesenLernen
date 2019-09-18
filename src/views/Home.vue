c6632b12709cde9e5af7a2efbc05f503ce79a74a
RchrdMgwtz <richard.mogwitz@htw-dresden.de>
2019-09-18 09:45:41 +0200

    Überschrift und Buttons der Startseite

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <!--<img alt="Vue logo" src="../assets/logo.png" />-->
     <!--<HelloWorld />-->
-    <BlogPost />
+    <!--<BlogPost />-->
+    <h1>Welche Buchstaben kannst du schon?</h1>
+    <button class="Normalbutton">Einstellungen</button>
+    <button class="Piktobutton">
+      <img id="puzzleimg" alt="Puzzle" src="../assets/puzzle.png" />
+    </button>
+    <button class="Piktobutton">
+      <img id="startimg" alt="Start" src="../assets/start.png" />
+    </button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 //import HelloWorld from "@/components/HelloWorld.vue";
-import BlogPost from "../components/Blogpost.vue";
+//import BlogPost from "../components/Blogpost.vue";
 
 export default {
   name: "Home",
   components: {
     //HelloWorld,
-    BlogPost
+    //BlogPost
   }
 };
 </script>
+
+<style>
+.Normalbutton {
+  background-color: lightgray;
+  border: none;
+  padding: 1em;
+  font-size: 1.2em;
+  border-radius: 4px;
+}
+
+.Piktobutton {
+  background-color: blue;
+  border-radius: 4px;
+  padding: 0px;
+}
+
+img {
+  height: 50%;
+  width: 50%;
+}
+</style>
